788e6e0ef95795762ac446e6c2b062bbd57388a3
RedRaceTeam <katrinkuzua12@gmail.com>
2026-08-19 19:54:31 +0700

    Update drivers.py

--- data/drivers.py
+++ data/drivers.py
@@ -6,203 +6,174 @@ DRIVERS = {
         "name": "Alex Palou",
         "team": "Chip Ganassi Racing",
         "number": 10,
-        "pos": 1,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/AlexPalou.png"
     },
     "KIR": {
         "name": "Kyle Kirkwood",
         "team": "Andretti Global",
         "number": 27,
-        "pos": 2,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/KyleKirkwood.png"
     },
     "MAL": {
         "name": "David Malukas",
         "team": "Team Penske",
         "number": 12,
-        "pos": 3,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/DavidMalukas.png"
     },
     "LUN": {
         "name": "Christian Lundgaard",
         "team": "Arrow McLaren",
         "number": 7,
-        "pos": 4,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/ChristianLundgaard.png"
     },
     "OWA": {
         "name": "Pato O'Ward",
         "team": "Arrow McLaren",
         "number": 5,
-        "pos": 5,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/PatoOWard.png"
     },
     "ROS": {
         "name": "Felix Rosenqvist",
         "team": "Meyer Shank Racing",
         "number": 60,
-        "pos": 6,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/FelixRosenqvist.png"
     },
     "NEW": {
         "name": "Josef Newgarden",
         "team": "Team Penske",
         "number": 2,
-        "pos": 7,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/JosefNewgarden.png"
     },
     "MCL": {
         "name": "Scott McLaughlin",
         "team": "Team Penske",
         "number": 3,
-        "pos": 8,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/ScottMcLaughlin.png"
     },
     "ERI": {
         "name": "Marcus Ericsson",
         "team": "Andretti Global",
         "number": 28,
-        "pos": 9,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/MarcusEricsson.png"
     },
     "VEE": {
         "name": "Rinus VeeKay",
         "team": "Juncos Hollinger Racing",
         "number": 76,
-        "pos": 10,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/RinusVeeKay.png"
     },
     "POW": {
         "name": "Will Power",
         "team": "Andretti Global",
         "number": 26,
-        "pos": 11,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/WillPower.png"
     },
     "DIX": {
         "name": "Scott Dixon",
         "team": "Chip Ganassi Racing",
         "number": 9,
-        "pos": 12,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/ScottDixon.png"
     },
     "RAH": {
         "name": "Graham Rahal",
         "team": "Rahal Letterman Lanigan Racing",
         "number": 15,
-        "pos": 13,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/GrahamRahal.png"
     },
     "SIM": {
         "name": "Kyffin Simpson",
         "team": "Chip Ganassi Racing",
         "number": 8,
-        "pos": 14,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/KyffinSimpson.png"
     },
     "ARM": {
         "name": "Marcus Armstrong",
         "team": "Meyer Shank Racing",
         "number": 66,
-        "pos": 15,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/MarcusArmstrong.png"
     },
     "ROSS": {
         "name": "Alexander Rossi",
         "team": "Ed Carpenter Racing",
         "number": 20,
-        "pos": 16,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/AlexanderRossi.png"
     },
     "FER": {
         "name": "Santino Ferrucci",
         "team": "A.J. Foyt Enterprises",
         "number": 14,
-        "pos": 17,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/SantinoFerrucci.png"
     },
     "FOS": {
         "name": "Louis Foster",
         "team": "Rahal Letterman Lanigan Racing",
         "number": 45,
-        "pos": 18,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/LouisFoster.png"
     },
     "SIE": {
         "name": "Nolan Siegel",
         "team": "Arrow McLaren",
         "number": 6,
-        "pos": 19,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/NolanSiegel.png"
     },
     "HAU": {
         "name": "Dennis Hauger",
         "team": "Dale Coyne Racing",
         "number": 19,
-        "pos": 20,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/DennisHauger.png"
     },
     "GRO": {
         "name": "Romain Grosjean",
         "team": "Dale Coyne Racing",
         "number": 18,
-        "pos": 21,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/RomainGrosjean.png"
     },
     "RAS": {
         "name": "Christian Rasmussen",
         "team": "Ed Carpenter Racing",
         "number": 21,
-        "pos": 22,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/ChristianRasmussen.png"
     },
     "COL": {
         "name": "Caio Collet",
         "team": "A.J. Foyt Enterprises",
         "number": 4,
-        "pos": 23,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/CaioCollet.png"
     },
     "SCH": {
         "name": "Mick Schumacher",
         "team": "Rahal Letterman Lanigan Racing",
         "number": 47,
-        "pos": 24,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/MickSchumacher.png"
     },
     "ROB": {
         "name": "Sting Ray Robb",
         "team": "Juncos Hollinger Racing",
         "number": 77,
-        "pos": 25,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/StingRayRobb.png"
     },
     "CAS": {
         "name": "Helio Castroneves",
         "team": "Meyer Shank Racing",
         "number": 6,
-        "pos": 30,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/HelioCastroneves.png"
     },
     "CAR": {
         "name": "Ed Carpenter",
         "team": "Ed Carpenter Racing",
         "number": 33,
-        "pos": 31,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/EdCarpenter.png"
     },
     "ILO": {
         "name": "Callum Ilott",
         "team": "PREMA Racing",
         "number": 90,
-        "pos": 32,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/CallumIlott.png"
     },
     "SHW": {
         "name": "Robert Shwartzman",
         "team": "PREMA Racing",
         "number": 83,
-        "pos": 33,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/RobertShwartzman.png"
     }
 }
